18f319cd37e5e9b5285545fbcb84ff5c4f3e1550
Alex Tsitsiura <s07641069@gmail.com>
2024-07-31 14:40:36 +0300

    [Telink] Use CurrentHue & CurrentSaturation as DeferredAttribute

--- examples/lighting-app/telink/src/AppTask.cpp
+++ examples/lighting-app/telink/src/AppTask.cpp
@@ -39,15 +39,19 @@ RgbColor_t sLedRgb;
 
 AppTask AppTask::sAppTask;
 
-// Define a custom attribute persister which makes actual write of the CurrentLevel attribute value
+// Define a custom attribute persister which makes actual write of the CurrentHue, CurrentSaturation, CurrentLevel attributes value
 // to the non-volatile storage only when it has remained constant for 5 seconds. This is to reduce
 // the flash wearout when the attribute changes frequently as a result of MoveToLevel command.
 // DeferredAttribute object describes a deferred attribute, but also holds a buffer with a value to
 // be written, so it must live so long as the DeferredAttributePersistenceProvider object.
-DeferredAttribute gCurrentLevelPersister(ConcreteAttributePath(kExampleEndpointId, Clusters::LevelControl::Id,
-                                                               Clusters::LevelControl::Attributes::CurrentLevel::Id));
+DeferredAttribute gPersisters[] = {
+    DeferredAttribute(ConcreteAttributePath(kExampleEndpointId, Clusters::ColorControl::Id, Clusters::ColorControl::Attributes::CurrentHue::Id)),
+    DeferredAttribute(ConcreteAttributePath(kExampleEndpointId, Clusters::ColorControl::Id, Clusters::ColorControl::Attributes::CurrentSaturation::Id)),
+    DeferredAttribute(ConcreteAttributePath(kExampleEndpointId, Clusters::LevelControl::Id, Clusters::LevelControl::Attributes::CurrentLevel::Id))
+
+};
 DeferredAttributePersistenceProvider gDeferredAttributePersister(Server::GetInstance().GetDefaultAttributePersister(),
-                                                                 Span<DeferredAttribute>(&gCurrentLevelPersister, 1),
+                                                                 Span<DeferredAttribute>(gPersisters, 3),
                                                                  System::Clock::Milliseconds32(5000));
 
 bool AppTask::IsTurnedOn() const
