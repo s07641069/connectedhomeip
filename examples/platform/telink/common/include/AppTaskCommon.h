67329403e17139e2fb77749eae2c38bfbcd9e7bb
Alex Tsitsiura <s07641069@gmail.com>
2025-02-06 03:51:00 +0200

    [Telink] Fix binary operations for OTA LZMA dual-mode (#373)
     - move merge of Zigbee binary from CMake into process_binaries.py .
     - add partition for Zigbee binary (TELINK_ZB_PARTITION_ADDR) instead of hardcoded values .
     - add the zigbee nvs partition for tl3218x .
     - adjust the zigbee nvs partition .
    
    Signed-off-by: Alex Tsitsiura <s07641069@gmail.com>
    Co-authored-by: Fengtai Xie <fengtai.xie@telink-semi.com>

--- examples/platform/telink/common/include/AppTaskCommon.h
+++ examples/platform/telink/common/include/AppTaskCommon.h
@@ -43,14 +43,8 @@
 #include <zephyr/storage/flash_map.h>
 #include <zephyr/sys/reboot.h>
 
-#if CONFIG_SOC_RISCV_TELINK_B92
-#define ZB_NVS_PARTITION zigbee_partition
+#define ZB_NVS_PARTITION zigbee_nvs_partition
 #define ZB_NVS_SEC_SIZE FIXED_PARTITION_SIZE(ZB_NVS_PARTITION)
-#elif CONFIG_SOC_RISCV_TELINK_TL321X
-#define ZB_NVS_PARTITION slot1_partition
-/* zb para locate in the slot1 , and it will cost 104k size in slot1 */
-#define ZB_NVS_SEC_SIZE (104 * 1024)
-#endif
 
 #define ZB_NVS_PARTITION_DEVICE FIXED_PARTITION_DEVICE(ZB_NVS_PARTITION)
 #define ZB_NVS_START_ADR FIXED_PARTITION_OFFSET(ZB_NVS_PARTITION)
